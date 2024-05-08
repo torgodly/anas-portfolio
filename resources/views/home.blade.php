645cc8ee2697451929e3b366efd504b2e937fece
ashebani <azizoo2001.as@gmail.com>
2024-05-08 19:33:27 +0200

    null

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,13 +18,16 @@
                 {{-- Experience Level --}}
                 <div
                     class="bg-white bg-opacity-15 border-4 grid relative border-white rounded-3xl md:w-[65%] p-6 text-black">
-                    <p class="text-white md:text-6xl sm:text-5xl text-3xl"><span class="text-snapchat-yellow md:text-8xl sm:text-7xl text-5xl font-bold">3
+                    <p class="text-white md:text-6xl sm:text-5xl text-3xl"><span
+                            class="text-snapchat-yellow md:text-8xl sm:text-7xl text-5xl font-bold">3
                         </span>Years
                         of Experience</p>
-                    <p class="text-white md:text-6xl sm:text-5xl text-3xl"><span class="text-snapchat-yellow md:text-8xl sm:text-7xl text-5xl font-bold">317</span>+
+                    <p class="text-white md:text-6xl sm:text-5xl text-3xl"><span
+                            class="text-snapchat-yellow md:text-8xl sm:text-7xl text-5xl font-bold">317</span>+
                         Million Plays</p>
                     <img src="{{ asset('assets/Snapchat-Rotated.png') }}"
-                        class="absolute md:w-40 w-20 md:right-[6%] sm:right-[6%] right-0 md:top-1/2 top-2/3 -translate-y-1/2" alt="">
+                        class="absolute md:w-40 w-20 md:right-[6%] sm:right-[6%] right-0 md:top-1/2 top-2/3 -translate-y-1/2"
+                        alt="">
                     <button
                         class="bg-white rounded-3xl md:px-12 md:py-4 px-10 py-4 mt-6 md:text-3xl text-lg font-bold justify-self-center text-black">See
                         Lenses
@@ -46,13 +49,17 @@
                     </div>
                     {{-- Social Media Links --}}
                     <div class="flex justify-between">
-                        <a href="#" class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
+                        <a href="#"
+                            class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
                                 src="{{ asset('assets/Snapchat.png') }}" alt=""></a>
-                        <a href="#" class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
+                        <a href="#"
+                            class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
                                 src="{{ asset('assets/Twitter.png') }}" alt=""></a>
-                        <a href="#" class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
+                        <a href="#"
+                            class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
                                 src="{{ asset('assets/Instagram.png') }}" alt=""></a>
-                        <a href="#" class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
+                        <a href="#"
+                            class="md:h-24 sm:h-[90px] h-[70px] bg-white rounded-2xl flex content-center p-4 aspect-square"><img
                                 src="{{ asset('assets/Discord.png') }}" alt=""></a>
                     </div>
                 </div>
@@ -63,8 +70,8 @@
 
             {{-- Snapchat --}}
             <div class="sm:p-10 p-8 rounded-2xl bg-snapchat-yellow bg-opacity-15 w-full relative">
-                <img src="{{ asset('assets/Lens.png') }}" class="absolute md:w-[70px] sm:w-20 w-16 left-5 top-1/2 -translate-y-1/2 z-10"
-                    alt="">
+                <img src="{{ asset('assets/Lens.png') }}"
+                    class="absolute md:w-[70px] sm:w-20 w-16 left-5 top-1/2 -translate-y-1/2 z-10" alt="">
                 <div class="flex items-center justify-center">
                     <div class="h-5 w-full overflow-hidden rounded-full bg-snapchat-yellow">
                         <div class="h-3 animate-pulse rounded-full bg-black w-[90%] m-1">
@@ -135,7 +142,8 @@
         </section>
 
         {{-- About me section --}}
-        <section class="my-20 rounded-2xl border-2 border-white sm:p-10 p-6 leading-loose sm:text-3xl text-lg text-center">
+        <section
+            class="my-20 rounded-2xl border-2 border-white sm:p-10 p-6 leading-loose sm:text-3xl text-lg text-center">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vel maxime quis itaque id obcaecati
             numquam, ad in nisi animi nulla aperiam nam beatae mollitia similique, exercitationem optio distinctio
             quisquam est? Porro temporibus ea deserunt labore voluptates sed atque sunt?
