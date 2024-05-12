11e8bb1f592de496c1c649ce0829e40d272ba1ef
Abdullah Alhaj <torgodly@gmail.com>
2024-05-12 14:39:59 +0200

    fix stuff

--- resources/views/components/social-media-icon.blade.php
+++ resources/views/components/social-media-icon.blade.php
@@ -1,4 +1,4 @@
 @props(['imageUrl', 'name', 'href' => '#'])
-<a class="flex aspect-square h-16 content-center rounded-2xl bg-white p-4 sm:h-[90px] md:h-16 xl:h-16"
+<a class="flex aspect-square h-20 content-center rounded-2xl bg-white p-4 sm:h-[90px] md:h-16 xl:h-20"
    href="{{ $href }}" target="_blank"><img src="{{ $imageUrl }}" alt="{{ $name }}"/>
 </a>
